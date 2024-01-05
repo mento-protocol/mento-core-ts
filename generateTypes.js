cc30ba36f0ae991ccd9bbe05bb549d3474087588
Bayological <6872903+bayological@users.noreply.github.com>
2024-01-05 11:25:18 +0000

    feat: update gen types to include new contracts (#9)
    ### Description
    
    - This PR updates the generate types script to include the new contracts that have been added in the recent Mento upgrades
    
    ### Other changes
    
    - None
    
    ### Tested
    
    - Ran script locally to verify new contract types were included as well as the existing types
    
    ### Related issues
    
    - https://github.com/mento-protocol/mento-sdk/issues/30
    
    ### Backwards compatibility
    
    Yes
    
    ### Documentation
    
    - N/A

--- generateTypes.js
+++ generateTypes.js
@@ -2,17 +2,20 @@ const { runTypeChain, glob } = require("typechain");
 const fs = require("fs");
 
 async function main() {
-  const cwd = process.cwd()
-  const interfaces = fs.readdirSync(
-    `${cwd}/lib/mento-core/contracts/interfaces`
-  );
-  // We generate types for some of the core contracts because they are
-  // useful in other packages, for example in the sdk.
-  const coreContracts = ['Broker.sol', 'BiPoolManager.sol']
-  const allContracts = interfaces.concat(coreContracts)
-  const allContractsPath = allContracts.map(
-    (contract) => `${contract}/${contract.replace('.sol', '.json')}`
-  )
+  const cwd = process.cwd();
+  const interfaces = fs.readdirSync(`${cwd}/lib/mento-core/contracts/interfaces`);
+
+  const governanceContracts = fs.readdirSync(`${cwd}/lib/mento-core/contracts/governance`);
+  const oracleContracts = fs.readdirSync(`${cwd}/lib/mento-core/contracts/oracles`);
+  const breakerContracts = fs.readdirSync(`${cwd}/lib/mento-core/contracts/oracles/breakers`);
+  const swapContracts = fs.readdirSync(`${cwd}/lib/mento-core/contracts/swap`);
+
+  const allContracts = interfaces
+    .concat(governanceContracts)
+    .concat(oracleContracts)
+    .concat(breakerContracts)
+    .concat(swapContracts);
+  const allContractsPath = allContracts.map(contract => `${contract}/${contract.replace(".sol", ".json")}`);
 
   const allFiles = glob(`${cwd}/out`, allContractsPath);
   await runTypeChain({
